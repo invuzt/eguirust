0f7e00bf91b96f5bf18377643ada1f0a928c8cf4
invuzt <invuzt@gmail.com>
2026-03-25 23:18:07 +0700

    fix: add ndk-glue and explicit lib name

--- src/lib.rs
+++ src/lib.rs
@@ -3,11 +3,16 @@ use macroquad::prelude::*;
 #[macroquad::main("OdfizApp")]
 async fn main() {
     loop {
-        clear_background(RED);
-
-        draw_text("BERHASIL!", 40.0, 100.0, 60.0, WHITE);
-        draw_text("Rust + Macroquad", 40.0, 160.0, 30.0, YELLOW);
+        // Coba warna hijau dulu (Green) untuk menandai versi baru
+        clear_background(GREEN);
         
+        draw_text("ODFIZ BERHASIL!", 40.0, 100.0, 50.0, BLACK);
+        draw_text("Tekan layar untuk keluar", 40.0, 160.0, 30.0, DARKGRAY);
+
+        if is_mouse_button_pressed(MouseButton::Left) {
+            break;
+        }
+
         next_frame().await
     }
 }
