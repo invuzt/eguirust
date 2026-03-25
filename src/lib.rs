96856b126b1a2d1b5a5c3595639e6a6f7bdfbc00
invuzt <invuzt@gmail.com>
2026-03-25 23:13:00 +0700

    fix: simplify macroquad setup to prevent force close

--- src/lib.rs
+++ src/lib.rs
@@ -3,13 +3,11 @@ use macroquad::prelude::*;
 #[macroquad::main("OdfizApp")]
 async fn main() {
     loop {
-        // Isi layar dengan warna merah terang
         clear_background(RED);
 
-        // Gambar teks di tengah layar
-        draw_text("HALO ODFIZ!", 20.0, 100.0, 60.0, WHITE);
-        draw_text("Rust is Running!", 20.0, 180.0, 30.0, YELLOW);
-
+        draw_text("BERHASIL!", 40.0, 100.0, 60.0, WHITE);
+        draw_text("Rust + Macroquad", 40.0, 160.0, 30.0, YELLOW);
+        
         next_frame().await
     }
 }
