45b64aff4feb05738748872d9425665291c3d66e
invuzt <invuzt@gmail.com>
2026-03-26 00:05:43 +0700

    fix: update input event handling for android-activity 0.6

--- src/lib.rs
+++ src/lib.rs
@@ -1,25 +1,29 @@
-use android_activity::{AndroidApp, MainEvent, PollEvent, InputEvent};
+use android_activity::{AndroidApp, MainEvent, PollEvent};
 use log::info;
 use std::time::Duration;
 
 #[no_mangle]
 fn android_main(app: AndroidApp) {
-    // Inisialisasi logger agar kita bisa lihat pesan di adb logcat
     android_logger::init_once(
         android_logger::Config::default().with_max_level(log::LevelFilter::Info),
     );
 
-    info!("Aplikasi Odfiz Native Dimulai!");
+    info!("Aplikasi Odfiz Native Dimulai! Coba sentuh layarnya.");
 
     loop {
+        // Kita ambil semua event yang masuk
         app.poll_events(Some(Duration::from_millis(16)), |event| {
             match event {
                 PollEvent::Main(MainEvent::Destroy) => {
-                    info!("Aplikasi ditutup oleh user.");
+                    info!("Aplikasi ditutup.");
                 }
-                PollEvent::Input(InputEvent::MotionEvent(motion_event)) => {
-                    let action = motion_event.action();
-                    info!("Layar disentuh! Action: {:?}", action);
+                // Cara baru akses input event di versi 0.6
+                PollEvent::Wake => {
+                    // Cek input secara manual dari queue
+                    let mut input_iter = app.input_events_iter().unwrap();
+                    while let Some(input_event) = input_iter.next() {
+                        info!("Ada input masuk ke HP: {:?}", input_event);
+                    }
                 }
                 _ => {}
             }
